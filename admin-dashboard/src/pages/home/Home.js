9e5d737c995131ad717cc47f5fabdd512eb3ff6e
Deepakgauttam <105534881+Deepakgauttam@users.noreply.github.com>
2023-03-06 00:01:46 +0530

    navbar and sidebar

--- admin-dashboard/src/pages/home/Home.js
+++ admin-dashboard/src/pages/home/Home.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Sidebar from '../../components/sidebar/Sidebar'
 import "./home.scss"
 const Home = () => {
   return (
-    <div className='home'>Home
+    <div className='home'>
+        <Sidebar/>
+        
     </div>
   )
 }
